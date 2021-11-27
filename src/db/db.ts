a53d96fdb22e138e33df72243d9b76e66bcc4c65
Tomer Eyal <tomereyal@gmail.com>
2021-11-27 17:30:20 +0200

    's'

--- src/db/db.ts
+++ src/db/db.ts
@@ -1,9 +1,20 @@
 import mysql2 from "mysql2/promise";
 
 export const db = mysql2.createPool({
-  host: process.env.DB_HOST || "localhost",
+  host: "us-cdbr-east-04.cleardb.com",
   port: Number(process.env.DB_PORT) | 3306,
-  user: process.env.DB_USER || "root",
-  password: process.env.DB_PASS || "mrtom45",
-  database: "trips",
+  user: "bb6b2620f61978",
+  password: "6e32c86b",
+  database: "heroku_98a56fc31c47ffd",
 });
+
+//FOR LOCAL DEVELOPMENT
+// export const db = mysql2.createPool({
+//   host: process.env.DB_HOST|| "localhost",
+//   port: Number(process.env.DB_PORT) | 3306,
+//   user: process.env.DB_USER || "root",
+//   password: process.env.DB_PASS || "mrtom45",
+//   database: "trips",
+// });
+
+//mysql://bb6b2620f61978:6e32c86b@us-cdbr-east-04.cleardb.com/heroku_98a56fc31c47ffd?reconnect=true
