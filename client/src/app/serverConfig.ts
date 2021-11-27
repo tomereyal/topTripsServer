a8611e1eb2ff96da9e07f73cad5f9d979ee7e565
Tomer Eyal <tomereyal@gmail.com>
2021-11-27 19:09:11 +0200

    s

--- client/src/app/serverConfig.ts
+++
@@ -1,6 +0,0 @@
-export const SERVER_BASE_URL =
-  process.env.PUBLIC_URL || `https://toptrips.herokuapp.com/api`;
-console.log(`SERVER_BASE_URL`, SERVER_BASE_URL);
-export const usersServerEndpoint = `${SERVER_BASE_URL}/users`;
-export const vacationsServerEndpoint = `${SERVER_BASE_URL}/vacations`;
-export const uploadsServerEndpoint = `${SERVER_BASE_URL}/uploads`;
--- client/src/hooks/useAuth.ts
+++
@@ -1,9 +0,0 @@
-import { useMemo } from "react";
-import { useSelector } from "react-redux";
-import { selectCurrentUser } from "../features/user/userSlice";
-
-export const useAuth = () => {
-  const user = useSelector(selectCurrentUser);
-
-  return useMemo(() => ({ user }), [user]);
-};
