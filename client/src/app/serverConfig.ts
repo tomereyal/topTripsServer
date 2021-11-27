0f7b363691e77f9c219bbae449f4f757e5e31dca
Tomer Eyal <tomereyal@gmail.com>
2021-11-27 17:52:51 +0200

    's'

--- client/src/app/serverConfig.ts
+++ client/src/app/serverConfig.ts
@@ -1,4 +1,5 @@
-const SERVER_BASE_URL = process.env.PUBLIC_URL || `http://localhost:5000/api`;
+const SERVER_BASE_URL =
+  process.env.PUBLIC_URL || `https://toptrips.herokuapp.com/api`;
 console.log(`SERVER_BASE_URL`, SERVER_BASE_URL);
 export const usersServerEndpoint = `${SERVER_BASE_URL}/users`;
 export const vacationsServerEndpoint = `${SERVER_BASE_URL}/vacations`;
